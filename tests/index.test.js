2b1efd1c19f269184b37544b9dded6f6b7a2d7a7
Arpit Dalal <arpitdalalm@gmail.com>
2024-04-22 14:20:27 -0400

    test: add test for referer working fine

--- tests/index.test.js
+++ tests/index.test.js
@@ -20,6 +20,18 @@ test('"/?utm_source=twitter&utm_medium=referrer&utm_campaign=social" redirects t
   );
 });
 
+test('"/" with referer as t.co redirects to "arpitdalal.dev/?utm_source=t.co&utm_medium=social&utm_campaign=url-shortener" with utm params', async () => {
+  const a = await fetch("http://localhost:3000", {
+    headers: {
+      referer: "https://t.co",
+    },
+  });
+
+  expect(a.url).toBe(
+    "https://arpitdalal.dev/?utm_source=t.co&utm_medium=social&utm_campaign=url-shortener"
+  );
+});
+
 test('"/does-not-exist" redirects to "arpitdalal.dev/does-not-exist" with utm params', async ({
   page,
 }) => {
